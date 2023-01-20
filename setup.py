071056d10df65b1aa6e3cd145d18379ad8be1776
Jvdboss7 <jaydeepdixit2@gmail.com>
2023-01-20 13:33:47 +0530

    folder structure added

--- setup.py
+++ setup.py
@@ -1,27 +1,10 @@
-import setuptools
+from setuptools import find_packages, setup
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-__version__ = "0.0.0"
-
-REPO_NAME = "FSDS_NOV_deepCNNClassifier"
-AUTHOR_USER_NAME = "c17hawke"
-SRC_REPO = "deepClassifier"
-AUTHOR_EMAIL = "sunny.c17hawke@gmail.com"
-
-setuptools.setup(
-    name=SRC_REPO,
-    version=__version__,
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description="A small python package for CNN app",
-    long_description=long_description,
-    long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+setup(
+    name="human-action-recognition",
+    version="0.0.1",
+    author="iNeuron and Jvdboss7",
+    author_email="cloud@ineuron.ai",
+    packages=find_packages(),
+    install_requires=[],
 )
